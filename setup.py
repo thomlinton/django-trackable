33f5edf57986a022b723fbcd54d7aec5268a761d
Thom Linton <thom.linton@gmail.com>
2010-09-14 16:24:01 -0700

    dreaming of a world where i already knew the ropes of pypi/setuptools/distutils

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires = [
         'django-celery',
         ],
-    long_description=read('README'),
+    long_description=read('README.rst'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
