cfa163e897df68e2265b444134078288f377fff7
Thom Linton <thom.linton@gmail.com>
2010-09-27 10:41:46 -0700

    Bumping version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "django-trackable",
-    version = "0.2.2",
+    version = "0.3.0",
     author = "Thom Linton",
     author_email = "thom.linton@gmail.com",
     description = "A Django application which provides tools for out-of-band capture of arbitrary tracking data.",
