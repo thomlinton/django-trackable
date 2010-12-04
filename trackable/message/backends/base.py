bfdb95843b3f6ae879db8aa53edcd6c9ed0e5e83
Thom Linton <thom.linton@gmail.com>
2010-12-03 22:19:34 -0800

    Major refactor of BaseMessageBackend

--- trackable/message/backends/base.py
+++ trackable/message/backends/base.py
@@ -1,20 +1,28 @@
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.mail import mail_admins
 from django.conf import settings
 
 from trackable.exceptions import TrackableError
 from trackable.models import Spider
 from trackable.sites import site
 
+import collections
 import datetime
 import warnings
 import logging
+import shelve
+import socket
+import errno
 
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
+BACKEND_CONNECTION_BACKOFF_SECONDS = getattr(settings,'TRACKABLE_BACKEND_CONNECTION_BACKOFF_SECONDS', 30)
+BACKEND_CONNECTION_LIMIT = getattr(settings,'TRACKABLE_BACKEND_CONNECTION_LIMIT', 5)
+BACKEND_SEND_QUEUE_SIZE = getattr(settings,'TRACKABLE_BACKEND_SEND_QUEUE', 1000)
 USER_AGENT_FILTERING = getattr(settings,'TRACKABLE_USER_AGENT_FILTERING', False)
 PROCESS_NUM_MESSAGES = getattr(settings,'TRACKABLE_PROCESS_NUM_MESSAGES', None)
 STRICT_MODE = getattr(settings,'TRACKABLE_STRICT_MODE', True)
@@ -22,16 +30,172 @@ LOGLEVEL = getattr(settings,'TRACKABLE_LOGLEVEL', logging.WARNING)
 MAGIC_FLAG = getattr(settings,'TRACKABLE_MAGIC_FLAG', '7R4CK4813M491C')
 
 UNDEFINED_AGENT = 'UNDEFINED_HTTP_USER_AGENT'
+SEND_QUEUE_PERSIST_NAME = 'trackable_send_queue.db'
 
+# configure default module logger
+_logger = logging.getLogger('trackable')
+_logger.setLevel(LOGLEVEL)
+_logger.addHandler(logging.StreamHandler())
+
+
+class MockMessage(object):
+    """ """
+    def __init__(self,payload={}):
+        self.payload = payload
+        self.payload['SOURCE'] = 'MockMessage'
+    def ack(self):
+        pass
 
 class BaseMessageBackend(object):
     """ """
+    def __init__(self):
+        #
+        # TODO: 
+        # Figure out if we need a hard limit (can we determine a bound)
+        # ... or perhaps we can just flush on overflow?
+        #
+        queue_kwargs = {}
+        try:
+            collections.deque.maxlen
+            queue_kwargs['maxlen'] = long(BACKEND_SEND_QUEUE_SIZE)
+        except AttributeError:
+            pass
+        self.send_queue = collections.deque(**queue_kwargs)
+        self.send_queue_loaded = False
+
+        self.connection_attempts = 0
+        self.connection_backoff_ts = None
+        self.connection_backoff_period = \
+            datetime.timedelta(seconds=BACKEND_CONNECTION_BACKOFF_SECONDS)
+        self.disconnected_notification_sent = False
+
+        self.connect() # avoid 'protected' _connect on __init__
+
+    def _readin_send_queue(self):
+        # default kwarg flag='c'
+        #   this mode will lock concurrent access
+        #   for both read & write (creating if necessary)
+        db = shelve.open(SEND_QUEUE_PERSIST_NAME)
+        for i in xrange(0,len(db)):
+            item_key = "%d" % (i)
+            self.send_queue.append( db[item_key] )
+            del db[item_key]
+        db.close()
+        self.send_queue_loaded = True
+
+    def _writeout_send_queue(self):
+        if len(self.send_queue):
+            # default kwarg flag='c'
+            #   this mode will lock concurrent access
+            #   for both read & write (creating if necessary)
+            db = shelve.open(SEND_QUEUE_PERSIST_NAME)
+            sq_range = xrange(0,len(self.send_queue))
+            db_range = sq_range
+            if not self.send_queue_loaded:
+                db_range = xrange(len(db),len(db)+len(self.send_queue))
+            _range = zip(db_range,sq_range)
+            for i,j in _range:
+                item_key = "%d" % (i)
+                db[item_key] = self.send_queue[j]
+                del self.send_queue[j]
+            db.close()
+
+    def get_connection(self):
+        """ """
+        raise NotImplementedError
+
+    def is_connected(self):
+        """
+        Determines whether or not the current instance should be
+        considered connected.
+
+        """
+        try:
+            test_connection = self.get_connection()
+        except socket.error:
+            return False
+        else:
+            if not test_connection:
+                return False
+            return True
+
+    def _connect(self):
+        if self.connection_attempts > BACKEND_CONNECTION_LIMIT:
+            if not self.connection_backoff_ts:
+                self.connection_backoff_ts = datetime.datetime.now()
+            if (datetime.datetime.now() - self.connection_backoff_ts) > self.connection_backoff_period:
+                self.connection_attempts = 0
+                self.connection_backoff_ts = None
+            else:
+                _logger.warning( \
+                    "Throttling connection attempt until %s" % ( \
+                        self.connection_backoff_ts + self.connection_backoff_period)
+                    )
+                return False
+
+        self.connect()
+        self.connection_attempts += 1
+        return True
+
+    def connect(self):
+        """
+        Handle the creation and/or negotiation of messages service.
+
+        """
+        try:
+            _logger.info( \
+                "Trying connection (%d previous attempts)" % (self.connection_attempts)
+                )
+            self.connection = self.get_connection()
+        except socket.error, e:
+            if settings.DEBUG:
+                raise e
+            if not self.disconnected_notification_sent:
+                mail_admins(
+                    '[django-trackable] Error while acquiring a connection in backend %s' % \
+                        (self.__class__), '%s' % (str(e))
+                    )
+                self.disconnected_notification_sent = True
+
+    def try_connect(self):
+        keep_trying = self._connect()
+        while not self.is_connected() and keep_trying:
+            keep_trying = self._connect()
+            if self.is_connected():
+                self.disconnected_notification_sent = False
+                _logger.info( \
+                    "Connection via backend %s acquired" % (self.__class__)
+                    )
+        self.connection_attempts = 0
+        self.connection_backoff_ts = None
+
+    def _send(self, payload):
+        self.try_connect()
+        if self.is_connected():
+            return self.send( payload )
+
+        self.send_queue.append(MockMessage(payload))
+        self._writeout_send_queue()
+
     def send(self, payload):
         """
         Publish ``payload`` to the message backend.
 
         """
         raise NotImplementedError
+
+    def _recv(self, load_send_queue=False):
+        # give priority to send_queue in the case
+        # that the call to self.try_connect will
+        # reacquire a connection.
+        if not self.send_queue_loaded and load_send_queue:
+            self._readin_send_queue()
+        if len(self.send_queue):
+            return self.send_queue.popleft()
+
+        self.try_connect()
+        if self.is_connected():
+            return self.recv()
 
     def recv(self):
         """
@@ -72,25 +236,31 @@ class BaseMessageBackend(object):
         if USER_AGENT_FILTERING:
             if message_obj['user_agent'] == UNDEFINED_AGENT:
                 msg = "Cannot match: user agent does not exist."
-                logger.warning( msg )
+                _logger.warning( msg )
                 message.ack()
                 return
             hits = Spider.objects.filter( \
                 user_agent__icontains=message_obj['user_agent'][:128])
             if hits:
                 msg = "Not processing potential spider-generated tracking message. User agent=%s" % (message_obj['user_agent'])
-                logger.warning( msg )
+                _logger.warning( msg )
                 message.ack()
                 return
 
-        self.send( message_obj )
+        self._send( message_obj )
 
-    def process_messages(self, logger=None, model_cls=None, max_messages=PROCESS_NUM_MESSAGES):
+    def process_messages(self, logger=None, model_cls=None, max_messages=PROCESS_NUM_MESSAGES, load_send_queue=False):
         """
         Process all currently gathered messages by compiling and 
         saving them to the database.
         
         """
+        if not logger:
+            logger = _logger
+
+        msg = "Send queue contains %d items" % (len(self.send_queue))
+        logger.info( msg )
+
         test_condition = lambda x: x < max_messages
         if not max_messages:
             test_condition = lambda x: True
@@ -100,20 +270,15 @@ class BaseMessageBackend(object):
         processed = 0
         cnt = 0
 
-        if not logger:
-            logger = logging.getLogger('trackable')
-            console = logging.StreamHandler()
-            logger.setLevel(LOGLEVEL)
-            logger.addHandler(console)
-
         while test_condition(processed):
-            message = self.recv()
+            message = self._recv(load_send_queue=load_send_queue)
             if not message:
                 break
 
             message_obj = message.payload
             if 'magic' not in message_obj or message_obj['magic'] != MAGIC_FLAG:
-                logger.warning( "Magic bytes not found or do not match. Skipping message." )
+                # logger.warning( "Magic bytes not found or do not match. Skipping message." )
+                message.ack()
                 continue
 
             logger.info( "%d Got message: %s" % (cnt+1,message_obj) )
@@ -134,6 +299,7 @@ class BaseMessageBackend(object):
             except ObjectDoesNotExist, e:
                 msg = "%s object with primary key %s does not exist" \
                     % (data_cls.__name__,data_object_pk)
+                logger.warning( msg )
                 if not STRICT_MODE:
                     continue
                 raise e
@@ -144,12 +310,14 @@ class BaseMessageBackend(object):
             except AttributeError, e:
                 msg = u"%s does not support %s operation." \
                     % (data_object,op_name)
+                logger.warning( msg )
                 if not STRICT_MODE:
                     continue
                 raise e
             except TrackableError, e:
                 msg = u"%s does not have an attribute %s" \
                     % (data_object,field_name)
+                logger.warning( msg )
                 if not STRICT_MODE:
                     continue
                 raise e
--- trackable/message/backends/carrot/base.py
+++ trackable/message/backends/carrot/base.py
@@ -1,57 +1,22 @@
-from django.core.mail import mail_admins
-from django.conf import settings
-
 from carrot.connection import DjangoBrokerConnection
-from carrot.messaging import Publisher, Consumer
+from carrot.messaging import Messaging
 
 from trackable.message.backends.base import BaseMessageBackend
 
 
 class MessageBackend(BaseMessageBackend):
     """ """
-    def __init__(self):
-        try:
-            self.connection = DjangoBrokerConnection()
-            self.consumer = Consumer( \
-                connection=self.connection,
-                queue='trackable_internal',
-                exchange='direct',
-                routing_key='trackable_internal',
-                )
-            self.publisher = Publisher( \
-                connection=self.connection,
-                queue='trackable_internal',
-                exchange='direct',
-                routing_key='trackable_internal',
-                serializer='pickle',
-                )
-        except Exception, e:
-            if settings.DEBUG:
-                raise e
-            mail_admins(
-                '[django-trackable] Error while acquiring a connection in carrot backend',
-                '%s' % (str(e))
-                )
-
+    def get_connection(self):
+        Messaging.publisher_cls.serializer = 'pickle'
+        return Messaging( \
+            connection=DjangoBrokerConnection(),
+            queue='trackable_internal',
+            exchange='direct',
+            routing_key='trackable_internal',
+            )
 
     def send(self, payload):
-        try:
-            return self.publisher.send( payload )
-        except Exception, e:
-            if settings.DEBUG:
-                raise e
-            mail_admins(
-                '[django-trackable] Error while sending message in carrot backend',
-                '%s' % (str(e))
-                )
+        return self.connection.send( payload )
 
     def recv(self):
-        try:
-            return self.consumer.fetch()
-        except Exception, e:
-            if settings.DEBUG:
-                raise e
-            mail_admins(
-                '[django-trackable] Error while sending message in carrot backend',
-                '%s' % (str(e))
-                )
+        return self.connection.fetch()
